edf40f5f0c80b540a92edb96e45c60ee258977b0
jespinol <76534045+jespinol@users.noreply.github.com>
2023-03-07 19:32:39 -0500

    created file containing frequently used constants, e.g. left, right, etc.

---
+++ constants.js
@@ -0,0 +1,38 @@
+// Game state constants
+const NEW = 'new';
+const RUNNING = 'running';
+const PAUSED = 'paused';
+const ENDING = 'ending';
+const ENDED = 'ended';
+const GAME_STATES = {
+  NEW, RUNNING, PAUSED, ENDING, ENDED,
+};
+
+// Stack state constants
+const UNCHANGED = 'unchanged';
+const UPDATING = 'updating';
+const NOT_UPDATABLE = 'not updatable';
+const STACK_STATES = { UNCHANGED, UPDATING, NOT_UPDATABLE };
+
+// Side constants
+const LEFT = 'left';
+const RIGHT = 'right';
+const UP = 'up';
+const DOWN = 'down';
+const SIDES = {
+  LEFT, RIGHT, UP, DOWN,
+};
+
+// Turn constants
+const CLOCKWISE_TURN = 'clockwise turn';
+const COUNTERCLOCKWISE_TURN = 'counter-clockwise turn';
+const TURNS = { CLOCKWISE_TURN, COUNTERCLOCKWISE_TURN };
+
+// Sound constants
+const BACKGROUND_MUSIC = new Audio('resources/static/sounds/TwisterTetris_poinl.mp3');
+
+const SOUNDS = { BACKGROUND_MUSIC };
+
+export {
+  GAME_STATES, STACK_STATES, SIDES, TURNS, SOUNDS,
+};
