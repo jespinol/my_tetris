da1c134dbfd2b90508686667a4260937d9b20a6c
jespinol <76534045+jespinol@users.noreply.github.com>
2023-03-08 21:45:33 -0500

    changed scoring rule to fixed number of rows to level up, minor refactoring

--- my_tetris.js
+++ my_tetris.js
@@ -5,7 +5,7 @@ import { GAME_STATES, SOUNDS, STACK_STATES } from './constants.js';
 const {
   NEW, RUNNING, PAUSED, ENDING, ENDED,
 } = GAME_STATES;
-const { UNCHANGED, UPDATING, NOT_UPDATABLE } = STACK_STATES;
+const { NOT_UPDATABLE } = STACK_STATES;
 const { BACKGROUND_MUSIC } = SOUNDS;
 
 export default class MyTetris {
@@ -16,8 +16,9 @@ export default class MyTetris {
     this.levelPoints = 0;
     this.level = 1;
     this.points = 0;
+    this.animationIsPlaying = false;
     this.animationReference = null;
-    this.lastRender = 0;
+    this.lastRender = null;
   }
 
   runGame(key) {
@@ -25,7 +26,9 @@ export default class MyTetris {
       case NEW: {
         SoundPlayer.play(BACKGROUND_MUSIC);
         const countdownFinished = this.gameField.drawCountdown();
-        countdownFinished ? this.gameState = RUNNING : NEW;
+        if (countdownFinished) {
+          this.gameState = RUNNING;
+        }
         break;
       }
       case RUNNING:
@@ -43,11 +46,12 @@ export default class MyTetris {
         }
         break;
       case PAUSED:
-        SoundPlayer.fadeAudioOut(BACKGROUND_MUSIC);
         cancelAnimationFrame(this.animationReference);
+        SoundPlayer.fadeAudioOut(BACKGROUND_MUSIC);
         this.gameField.drawPausedMessage();
         break;
       case ENDING:
+        cancelAnimationFrame(this.animationReference);
         SoundPlayer.pause(BACKGROUND_MUSIC);
         this.gameField.drawGameOverMessage();
         this.gameState = ENDED;
@@ -68,12 +72,12 @@ export default class MyTetris {
   }
 
   updatePoints() {
-    const clearedRows = this.gameField.checkRows();
+    const clearedRows = this.gameField.checkClearedRows();
     if (clearedRows > 0) {
       this.levelPoints += clearedRows;
       this.points += clearedRows * 100 * this.level;
       document.getElementById('score').innerText = (this.points).toString();
-      return this.levelPoints >= (5 * this.level);
+      return this.levelPoints >= (10 * this.level);
     }
     return false;
   }
