46b396c1dc2a66a8bc7d2a5385268307d0576f2f
jespinol <76534045+jespinol@users.noreply.github.com>
2023-03-11 20:18:32 -0500

    added some rule exceptions to linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,9 @@
 module.exports = {
-    "extends": [
-        "airbnb"
-    ],
-    parserOptions: {
-        ecmaVersion: 2022,
+    extends: 'airbnb',
+    env: {'browser': true},
+    parserOptions: {ecmaVersion: 2022,},
+    rules: {
+        'import/extensions': 'off',
+        'no-plusplus': ['error', {'allowForLoopAfterthoughts': true}]
     }
 }
