5cae1dc545e2e5bb4f1560bd01faed2e8e0c28d9
jespinol <76534045+jespinol@users.noreply.github.com>
2023-03-15 01:27:15 -0400

    refactored code for added simplicity and readability

--- init.js
+++ init.js
@@ -6,14 +6,12 @@ const {
   NEW, RUNNING, PAUSED, ENDED,
 } = GAME_STATES;
 
-function setCanvas(canvasId, width, height) {
-  document.getElementById(canvasId).setAttribute('width', width);
-  document.getElementById(canvasId).setAttribute('height', height);
+function resizeCanvas(canvas, width, height) {
+  canvas.setAttribute('width', width);
+  canvas.setAttribute('height', height);
 }
 
-function getBlockSizeSetCanvas() {
-  const blockSize = Math.round((window.innerHeight * 0.1) / 10) * 10;
-
+function setCanvas(blockSize, gameCanvas, nextCanvas, holdCanvas) {
   const blocksInRow = blockSize * Board.columns;
   const gameCanvasWidth = blocksInRow.toString();
   const blocksInCol = blockSize * Board.rows;
@@ -21,18 +19,22 @@ function getBlockSizeSetCanvas() {
   const blocksInSide = blocksInRow * (40 / 100);
   const sideCanvasSize = blocksInSide.toString();
 
-  setCanvas('gameCanvas', gameCanvasWidth, gameCanvasHeight);
-  setCanvas('nextCanvas', sideCanvasSize, sideCanvasSize);
-  setCanvas('holdCanvas', sideCanvasSize, sideCanvasSize);
-  return blockSize;
+  resizeCanvas(gameCanvas, gameCanvasWidth, gameCanvasHeight);
+  resizeCanvas(nextCanvas, sideCanvasSize, sideCanvasSize);
+  resizeCanvas(holdCanvas, sideCanvasSize, sideCanvasSize);
+}
+
+function getBlockSize() {
+  return Math.round((window.innerHeight * 0.1) / 10) * 10;
 }
 
 export default function init() {
-  const playButton = document.getElementById('playButton');
-  const gameCanvas = document.getElementById('gameCanvas');
-  const holdCanvas = document.getElementById('holdCanvas');
-  const nextCanvas = document.getElementById('nextCanvas');
-  const blockSize = getBlockSizeSetCanvas();
+  const playButton = document.querySelector('#playButton');
+  const gameCanvas = document.querySelector('#gameCanvas');
+  const holdCanvas = document.querySelector('#holdCanvas');
+  const nextCanvas = document.querySelector('#nextCanvas');
+  const blockSize = getBlockSize();
+  setCanvas(blockSize, gameCanvas, nextCanvas, holdCanvas);
   const game = new MyTetris(gameCanvas, nextCanvas, holdCanvas, blockSize, playButton);
 
   playButton.addEventListener('click', () => {
@@ -58,16 +60,23 @@ export default function init() {
     const key = event.code;
     if (event.defaultPrevented) {
       return;
-    } if (game.gameState === RUNNING) {
-      if (key === 'Escape' || key === 'F1' || key === 'KeyP') {
+    }
+    switch (key) {
+      case 'Escape':
+      case 'F1':
+      case 'KeyP':
         game.switchGameStates(PAUSED);
-      } else {
-        game.runGame(key);
-      }
-    } else if (game.gameState === PAUSED) {
-      if (key === 'Enter') {
-        game.switchGameStates(RUNNING);
-      }
+        break;
+      case 'Enter':
+        if (game.gameState === PAUSED) {
+          game.switchGameStates(RUNNING);
+        }
+        break;
+      default:
+        if (game.gameState === RUNNING) {
+          game.runGame(key);
+        }
+        break;
     }
     event.preventDefault();
   }, true);
